3f7224b5d9e4976bf49c38948d839b96461a9a36
duoduo <sduoduo233@gmail.com>
2022-03-31 09:17:35 +0800

    注入器

---
+++ Injector/MainForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Injector
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_refresh = new System.Windows.Forms.Button();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.btn_choose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btn_inject = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.AccessibleName = "btn_refresh";
+            this.btn_refresh.Location = new System.Drawing.Point(12, 12);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(94, 29);
+            this.btn_refresh.TabIndex = 0;
+            this.btn_refresh.Text = "刷新";
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            this.btn_refresh.Click += new System.EventHandler(this.btn_refresh_Click);
+            // 
+            // comboBox
+            // 
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(112, 13);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(151, 28);
+            this.comboBox.TabIndex = 2;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(58, 47);
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(205, 27);
+            this.textBox.TabIndex = 3;
+            // 
+            // btn_choose
+            // 
+            this.btn_choose.Location = new System.Drawing.Point(269, 47);
+            this.btn_choose.Name = "btn_choose";
+            this.btn_choose.Size = new System.Drawing.Size(94, 29);
+            this.btn_choose.TabIndex = 4;
+            this.btn_choose.Text = "浏览";
+            this.btn_choose.UseVisualStyleBackColor = true;
+            this.btn_choose.Click += new System.EventHandler(this.btn_choose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "DLL:";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "DLL|*.dll|所有文件|*.*";
+            // 
+            // btn_inject
+            // 
+            this.btn_inject.Location = new System.Drawing.Point(269, 13);
+            this.btn_inject.Name = "btn_inject";
+            this.btn_inject.Size = new System.Drawing.Size(94, 29);
+            this.btn_inject.TabIndex = 6;
+            this.btn_inject.Text = "注入";
+            this.btn_inject.UseVisualStyleBackColor = true;
+            this.btn_inject.Click += new System.EventHandler(this.btn_inject_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 77);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(58, 20);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Github";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 118);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btn_inject);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_choose);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.btn_refresh);
+            this.Name = "MainForm";
+            this.Text = "LunarInputFix";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btn_refresh;
+        private ComboBox comboBox;
+        private TextBox textBox;
+        private Button btn_choose;
+        private Label label1;
+        private OpenFileDialog openFileDialog;
+        private Button btn_inject;
+        private LinkLabel linkLabel1;
+    }
+}
---
+++ Injector/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Injector
+{
+    public partial class MainForm : Form
+    {
+
+        private List<int> pids = new List<int>();
+        
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_refresh_Click(object sender, EventArgs e)
+        {
+            pids.Clear();
+            comboBox.Items.Clear();
+            foreach (Process process in Process.GetProcessesByName("javaw"))
+            {
+                pids.Add(process.Id);
+                comboBox.Items.Add($"{process.Id} - {process.MainWindowTitle}");
+            }
+            foreach (Process process in Process.GetProcessesByName("java"))
+            {
+                pids.Add(process.Id);
+                comboBox.Items.Add($"{process.Id} - {process.MainWindowTitle}");
+            }
+        }
+
+        private void btn_choose_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btn_inject_Click(object sender, EventArgs e)
+        {
+            if (comboBox.SelectedIndex < 0)
+                return;
+            int pid = pids[comboBox.SelectedIndex];
+            Injector.InjectDLL(pid, textBox.Text);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("rundll32.exe", "url.dll,FileProtocolHandler https://github.com/sduoduo233/LunarInputFix/");
+        }
+    }
+}
